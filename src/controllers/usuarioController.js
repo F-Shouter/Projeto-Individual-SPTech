6dc2195e2f2894e93395d64ba9651cf0b988b5c2
F-Shouter <yukari.yamasaki1809@gmail.com>
2025-04-30 14:25:02 -0300

    C:/Program Files/Git/controllers/usuarioController atualizada

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -23,29 +23,28 @@ function cadastrar(req, res) {
             );
     }
 
-    function retornar(req, res) {
-        // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
-        var apelido_jogador1 = req.body.apelido_jogador1Server;
-        var apelido_jogador2 = req.body.apelido_jogador2Server;
-       
+function retornar(req, res) {
+    // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
+    var apelido_jogador1 = req.body.apelido_jogador1Server;
+    var apelido_jogador2 = req.body.apelido_jogador2Server;
     
-            // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-            usuarioModel.retornar(apelido_jogador1, apelido_jogador2)
-                .then(
-                    function (resultado) {
-                        res.json(resultado);
-                    }
-                ).catch(
-                    function (erro) {
-                        console.log(erro);
-                        console.log(
-                            "\nHouve um erro ao realizar o cadastro! Erro: ",
-                            erro.sqlMessage
-                        );
-                        res.status(500).json(erro.sqlMessage);
-                    }
-                );
-        }
+        // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
+        usuarioModel.retornar(apelido_jogador1, apelido_jogador2)
+            .then(
+                function (resultado) {
+                    res.json(resultado);
+                }
+            ).catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log(
+                        "\nHouve um erro ao realizar o retorno! Erro: ",
+                        erro.sqlMessage
+                    );
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+    }
 
 module.exports = {
     cadastrar,
