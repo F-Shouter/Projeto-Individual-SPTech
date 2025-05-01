f04471a80bf00d5adef7cbaf969f700344e9fd66
F-Shouter <yukari.yamasaki1809@gmail.com>
2025-05-01 18:07:04 -0300

    app.js com grafico_motivo adicionado

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var app = express();
 
 var usuarioRouter = require("./src/routes/usuarios");
 var grafico_afastamentoRouter = require("./src/routes/grafico_afastamento");
+var grafico_motivoRouter = require("./src/routes/grafico_motivo");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -22,6 +23,7 @@ app.use(cors());
 
 app.use("/usuarios", usuarioRouter);
 app.use("/grafico_afastamento", grafico_afastamentoRouter);
+app.use("/grafico_motivo", grafico_motivoRouter);
 
 app.listen(PORTA_APP, function () {
     console.log(`
