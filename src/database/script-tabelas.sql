d2dbe3806c860bf1d84cbf0d5b45a7f422ccb88f
F-Shouter <yukari.yamasaki1809@gmail.com>
2025-05-27 19:53:06 -0300

    script BD refeita concluída

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -1,39 +1,116 @@
--- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
--- Você precisa executar os comandos no banco de dados para criar as tabelas,
--- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
+CREATE DATABASE Draw;
+USE Draw;
 
-/*
-comandos para mysql server
-*/
-
-CREATE DATABASE draw_With_Me;
-
-USE draw_With_Me;
-
-CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	apelido_jogador1 VARCHAR(20),
-	apelido_jogador2 VARCHAR(20),
-	avatar_jogador1 VARCHAR(45),
-	avatar_jogador2 VARCHAR(45)
-) AUTO_INCREMENT = 1;
-
-CREATE TABLE grafico_afastamento (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	qtd_sim int,
-	qtd_nao int	
+CREATE TABLE Jogador(
+	idJogador INT PRIMARY KEY AUTO_INCREMENT,
+    apelido VARCHAR(12),
+    avatar VARCHAR(45),
+    fkParceiro INT,
+    FOREIGN KEY (fkParceiro) REFERENCES Jogador(idJogador)
 ) AUTO_INCREMENT = 100;
 
-CREATE TABLE grafico_motivo (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-    bullying int,
-    problema_em_casa int,
-    mau_entendido int,
-    outro int	
-) AUTO_INCREMENT = 200;
+CREATE TABLE Pesquisa(
+	idPesquisa INT PRIMARY KEY AUTO_INCREMENT,
+    tema VARCHAR(45)
+);
 
-CREATE TABLE grafico_emocional (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-    qtd_concordam int,
-    qtd_discordam int		
-) AUTO_INCREMENT = 300;
+CREATE TABLE Resposta (
+    idResposta INT AUTO_INCREMENT,
+    data DATE DEFAULT (CURRENT_DATE),
+    opcao VARCHAR(45) CHECK (
+        opcao IN ('sim', 'não', 'problema_casa', 'bullying', 'mau_entendido', 'outro')),
+    fkPesquisa INT,
+    fkJogador INT,
+	PRIMARY KEY (idResposta, fkPesquisa, fkJogador),
+    FOREIGN KEY (fkPesquisa) REFERENCES Pesquisa(idPesquisa),
+    FOREIGN KEY (fkJogador) REFERENCES Jogador(idJogador)
+) AUTO_INCREMENT = 1000;
+
+INSERT INTO Pesquisa VALUES 
+	(DEFAULT, "Isolamento durante Infância"),
+    (DEFAULT, "Motivo de Afastamento"),
+    (DEFAULT, "Prejuízo Emocional");
+    
+INSERT INTO Jogador VALUES
+	(DEFAULT, "ADMIN", NULL, NULL);
+    
+-- ------------------- ATUALIZAÇÃO NO BANCO COM OS DADOS OBTIDOS DAS PESQUISAS -------------------------    
+-- 42 respostas "sim" para idPesquisa = 1
+INSERT INTO Resposta (opcao, fkPesquisa, fkJogador) VALUES
+('sim', 1, 100), ('sim', 1, 100), ('sim', 1, 100), ('sim', 1, 100), ('sim', 1, 100),
+('sim', 1, 100), ('sim', 1, 100), ('sim', 1, 100), ('sim', 1, 100), ('sim', 1, 100),
+('sim', 1, 100), ('sim', 1, 100), ('sim', 1, 100), ('sim', 1, 100), ('sim', 1, 100),
+('sim', 1, 100), ('sim', 1, 100), ('sim', 1, 100), ('sim', 1, 100), ('sim', 1, 100),
+('sim', 1, 100), ('sim', 1, 100), ('sim', 1, 100), ('sim', 1, 100), ('sim', 1, 100),
+('sim', 1, 100), ('sim', 1, 100), ('sim', 1, 100), ('sim', 1, 100), ('sim', 1, 100),
+('sim', 1, 100), ('sim', 1, 100), ('sim', 1, 100), ('sim', 1, 100), ('sim', 1, 100),
+('sim', 1, 100), ('sim', 1, 100), ('sim', 1, 100), ('sim', 1, 100), ('sim', 1, 100),
+('sim', 1, 100), ('sim', 1, 100);
+
+-- 8 respostas "não" para idPesquisa = 1
+INSERT INTO Resposta (opcao, fkPesquisa, fkJogador) VALUES
+('não', 1, 100), ('não', 1, 100), ('não', 1, 100), ('não', 1, 100),
+('não', 1, 100), ('não', 1, 100), ('não', 1, 100), ('não', 1, 100);
+
+-- 15 respostas "bullying" para idPesquisa = 2
+INSERT INTO Resposta (opcao, fkPesquisa, fkJogador) VALUES
+('bullying', 2, 100), ('bullying', 2, 100), ('bullying', 2, 100), ('bullying', 2, 100), ('bullying', 2, 100),
+('bullying', 2, 100), ('bullying', 2, 100), ('bullying', 2, 100), ('bullying', 2, 100), ('bullying', 2, 100),
+('bullying', 2, 100), ('bullying', 2, 100), ('bullying', 2, 100), ('bullying', 2, 100), ('bullying', 2, 100);
+
+-- 10 respostas "outro" para idPesquisa = 2
+INSERT INTO Resposta (opcao, fkPesquisa, fkJogador) VALUES
+('outro', 2, 100), ('outro', 2, 100), ('outro', 2, 100), ('outro', 2, 100), ('outro', 2, 100),
+('outro', 2, 100), ('outro', 2, 100), ('outro', 2, 100), ('outro', 2, 100), ('outro', 2, 100);
+
+-- 10 respostas "mau_entendido" para idPesquisa = 2
+INSERT INTO Resposta (opcao, fkPesquisa, fkJogador) VALUES
+('mau_entendido', 2, 100), ('mau_entendido', 2, 100), ('mau_entendido', 2, 100), ('mau_entendido', 2, 100), ('mau_entendido', 2, 100),
+('mau_entendido', 2, 100), ('mau_entendido', 2, 100), ('mau_entendido', 2, 100), ('mau_entendido', 2, 100), ('mau_entendido', 2, 100);
+
+-- 7 respostas "problema_casa" para idPesquisa = 2
+INSERT INTO Resposta (opcao, fkPesquisa, fkJogador) VALUES
+('problema_casa', 2, 100), ('problema_casa', 2, 100), ('problema_casa', 2, 100), ('problema_casa', 2, 100),
+('problema_casa', 2, 100), ('problema_casa', 2, 100), ('problema_casa', 2, 100);
+
+-- 39 respostas "sim" para idPesquisa = 3
+INSERT INTO Resposta (opcao, fkPesquisa, fkJogador) VALUES
+('sim', 3, 100), ('sim', 3, 100), ('sim', 3, 100), ('sim', 3, 100), ('sim', 3, 100),
+('sim', 3, 100), ('sim', 3, 100), ('sim', 3, 100), ('sim', 3, 100), ('sim', 3, 100),
+('sim', 3, 100), ('sim', 3, 100), ('sim', 3, 100), ('sim', 3, 100), ('sim', 3, 100),
+('sim', 3, 100), ('sim', 3, 100), ('sim', 3, 100), ('sim', 3, 100), ('sim', 3, 100),
+('sim', 3, 100), ('sim', 3, 100), ('sim', 3, 100), ('sim', 3, 100), ('sim', 3, 100),
+('sim', 3, 100), ('sim', 3, 100), ('sim', 3, 100), ('sim', 3, 100), ('sim', 3, 100),
+('sim', 3, 100), ('sim', 3, 100), ('sim', 3, 100), ('sim', 3, 100), ('sim', 3, 100),
+('sim', 3, 100), ('sim', 3, 100), ('sim', 3, 100), ('sim', 3, 100);
+
+-- 4 respostas "não" para idPesquisa = 3
+INSERT INTO Resposta (opcao, fkPesquisa, fkJogador) VALUES
+('não', 3, 100), ('não', 3, 100), ('não', 3, 100), ('não', 3, 100);
+
+-- ---------------------------- CRIAÇÃO DAS VIEWS ------------------------------------------
+CREATE VIEW view_resultado_afastamento AS
+SELECT 
+    SUM(CASE WHEN r.opcao = 'sim' THEN 1 ELSE 0 END) AS qtd_sim,
+    SUM(CASE WHEN r.opcao = 'não' THEN 1 ELSE 0 END) AS qtd_nao
+FROM Resposta AS r
+JOIN Pesquisa AS p ON r.fkPesquisa = p.idPesquisa
+WHERE r.fkPesquisa = 1;
+
+CREATE VIEW view_resultado_motivo AS
+SELECT 
+    SUM(CASE WHEN r.opcao = 'bullying' THEN 1 ELSE 0 END) AS bullying,
+    SUM(CASE WHEN r.opcao = 'problema_casa' THEN 1 ELSE 0 END) AS problema_casa,    
+    SUM(CASE WHEN r.opcao = 'mau_entendido' THEN 1 ELSE 0 END) AS mau_entendido,
+    SUM(CASE WHEN r.opcao = 'outro' THEN 1 ELSE 0 END) AS outro
+FROM Resposta AS r
+JOIN Pesquisa AS p ON r.fkPesquisa = p.idPesquisa
+WHERE r.fkPesquisa = 2;
+
+CREATE VIEW view_resultado_emocional AS
+SELECT 
+    SUM(CASE WHEN r.opcao = 'sim' THEN 1 ELSE 0 END) AS concordam,
+    SUM(CASE WHEN r.opcao = 'não' THEN 1 ELSE 0 END) AS discordam
+FROM Resposta AS r
+JOIN Pesquisa AS p ON r.fkPesquisa = p.idPesquisa
+WHERE r.fkPesquisa = 3;
